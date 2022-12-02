36d1d0dc39117321cf4fe82de47c35107c08ad0c
HarryWang <50002801+hapsdoifh@users.noreply.github.com>
2022-12-01 23:03:54 -0500

    Added FileIO files

---
+++ FileIO/FileOP.java
@@ -0,0 +1,15 @@
+package FileIO;
+
+import java.util.ArrayList;
+
+public class FileOP {
+    private ArrayList<String> Client;
+    private ArrayList<String> passwords;
+    public int readFile(String FileName){
+        return 0;
+    }
+    public int writeFile(){
+        return 0;
+    }
+
+}
