ed7cd024d88ce986cdbe7f7d2d51ba93160a731a
HarryWang <zuhanwang@outlook.com>
2022-12-02 11:30:14 -0500

    Doesn't Work

--- FileIO/FileOP.java
+++ FileIO/FileOP.java
@@ -1,11 +1,28 @@
 package FileIO;
 
 import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
 
 public class FileOP {
     private ArrayList<String> Client;
     private ArrayList<String> passwords;
+
     public int readFile(String FileName){
+        File fileIn = new File(FileName);
+        if(fileIn.exists()){
+            System.out.println("yes");
+        }
+        try{
+            Scanner sc = new Scanner(fileIn);
+            while(sc.hasNextLine()){
+                String temp = sc.nextLine();
+                int split = temp.indexOf(":");
+                Client.add(temp.substring(0,split));
+                passwords.add(temp.substring(split+1));
+            }
+            
+        }catch(Exception e){}
         return 0;
     }
     public int writeFile(){
---
+++ FileIO/Main.java
@@ -0,0 +1,9 @@
+
+package FileIO;
+
+public class Main{
+    public static void main(String[] args){
+        FileOP textOp = new FileOP();
+        textOp.readFile("FioIO\\Info.txt");
+    }
+}
