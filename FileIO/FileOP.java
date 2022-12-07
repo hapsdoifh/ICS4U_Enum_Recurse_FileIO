0b4977374e0302f0f7e55205fe88cd121033405d
HarryWang <50002801+hapsdoifh@users.noreply.github.com>
2022-12-06 19:01:03 -0500

    Finish FileIO

--- FileIO/FileOP.java
+++ FileIO/FileOP.java
@@ -6,7 +6,7 @@ import java.io.*;
 
 public class FileOP {
     private ArrayList<String> Client = new ArrayList<>();
-    private ArrayList<String> passwords = new ArrayList<>();
+    private ArrayList<String> Passwords = new ArrayList<>();
 
     public int readFile(String FileName){
         try{
@@ -16,13 +16,41 @@ public class FileOP {
                 String temp = sc.nextLine();
                 int split = temp.indexOf(":");
                 Client.add(temp.substring(0,split));
-                passwords.add(temp.substring(split+1));
+                Passwords.add(temp.substring(split+1));
             }
+            fIn.close();
         }catch(Exception e){}
         return 0;
     }
-    public int writeFile(){
+
+    public int writeFile(String FileName){
+        try{
+            FileWriter fOut = new FileWriter(FileName);
+            for(int i = 0; i<Client.size(); i++){
+                fOut.write(Client.get(i)+":"+Passwords.get(i));
+                if(i+1 < Client.size()){
+                    fOut.write("\n");
+                }
+            }
+            fOut.close();
+        }catch(Exception e){}
         return 0;
     }
 
+    public void AddClient(String Name, String pass){
+        Client.add(Name);
+        Passwords.add(pass);
+    }
+    public void RemoveClient(int position){
+        try{
+            Client.remove(position-1);
+            Passwords.remove(position-1);
+        }catch(Exception e){}
+    }
+
+    public void PrintList(){
+        for(int i = 0; i < Client.size(); i++){
+            System.out.println((i+1) + ". Name: " + Client.get(i) + "\t\tPassword: " + Passwords.get(i));
+        }
+    }
 }
--- FileIO/Main.java
+++ FileIO/Main.java
@@ -1,9 +1,41 @@
 
 package FileIO;
 
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
         FileOP textOp = new FileOP();
+        Scanner keyboard = new Scanner(System.in);
         textOp.readFile("FileIO\\Info.txt");
+        String tempN="",tempP="",Action = "";
+        while(!Action.equals("exit")){
+            System.out.print("Add new client name (\"skip\" to actions): ");
+            tempN = keyboard.nextLine();
+            if(!tempN.equals("skip")){
+                System.out.print("Add new client password (\"skip\" to actions): ");
+                tempP = keyboard.nextLine();
+                if(!tempP.equals("skip")){
+                    textOp.AddClient(tempN, tempP);
+                }
+            }   
+            System.out.print("\nChoose action (\"exit\", \"view\",\"remove\", any key to continue): ");
+            Action = keyboard.nextLine();
+            switch(Action){
+                case "view":
+                    System.out.println("Client List: ");
+                    textOp.PrintList();
+                    System.out.println();
+                    break; 
+                case "exit":
+                    textOp.writeFile("FileIO\\Info.txt");
+                    break;
+                case "remove":                    
+                    System.out.println("Which client? (number): ");
+                    textOp.RemoveClient(keyboard.nextInt());
+                    break;
+                default:
+            }
+        }
     }
 }
