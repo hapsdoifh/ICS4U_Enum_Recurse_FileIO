9a4cead0304778cb6b884f92779ddd346781e1f3
HarryWang <50002801+hapsdoifh@users.noreply.github.com>
2022-12-07 22:07:52 -0500

    allowed input for recursive algorithm

--- Recursion/Main.java
+++ Recursion/Main.java
@@ -1,7 +1,16 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
-        System.out.println(IncrementAdder.AddSequence(9, 3));
+        Scanner keyboard = new Scanner(System.in);
+        int end,step;
+        System.out.println("Enter end value: ");
+        end = keyboard.nextInt();
+        System.out.println("Enter step value: ");
+        step = keyboard.nextInt();
+        
+        System.out.println(IncrementAdder.AddSequence(end, step));
     }
 }
